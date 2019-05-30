c8ecc81247c8fe6ae8ead8edb4e2ece53d7fd021
chrismaynard64 <chrismaynard64@gmail.com>
2019-05-31 00:31:48 +0100

    0.0.2

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -8,55 +8,55 @@ export const teams = [
 
 export const players = [
     {id: 1, fname: 'Nikita', lname: 'Parris', age: 22, height: '5-4', caps: 20, goals: 0, 
-            picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/nikita-parris-723.ashx', 
+            picurl: 'assets/images/nikita-parris-723.png', 
             club: 'Manchester City', dob:'1994-03-10', team: 'England', position: 'Forward'},
    
     {id: 2, fname: 'Fran',   lname: 'Kirby',  age: 22, height: '5-2', caps: 38, goals: 0,
-             picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/fran-kirby-723.ashx', 
+             picurl: 'assets/images/fran-kirby-723.png', 
              club: 'Chelsea', dob:'1993-06-29', team: 'England', position: 'Forward'},
   
     {id: 3, fname: 'Karen',   lname: 'Bardsley',  age: 34, height: '5-11', caps: 77, goals: 0,
-              picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/karen-bardsley-723.ashx', 
+              picurl: 'assets/images/karen-bardsley-723.png', 
               club: 'Manchester City', dob:'1993-06-29', team: 'England', position: 'Goalkeeper'},
  
     {id: 4, fname: 'Alex',   lname: 'Greenwood',  age: 25, height: '5-6', caps: 38, goals: 0,
-               picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/alex-greenwood-723.ashx', 
+               picurl: 'assets/images/alex-greenwood-723.png', 
                club: 'Manchester United', dob:'1993-06-29', team: 'England', position: 'Defender'},
 
     {id: 5, fname: 'Lucy',   lname: 'Bronze',  age: 27, height: '5-8', caps: 38, goals: 0,
-                picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/lucy-bronze-723.ashx', 
+                picurl: 'assets/images/lucy-bronze-723.png', 
                 club: 'Olympique Lyonnais', dob:'1993-06-29', team: 'England', position: 'Defender'},
 
     {id: 6, fname: 'Demi',   lname: 'Stokes',  age: 27, height: '5-4', caps: 51, goals: 0,
-                    picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/demi-stokes-723.ashx', 
+                    picurl: 'assets/images/demi-stokes-723.png', 
                     club: 'Manchester City', dob:'1993-06-29', team: 'England', position: 'Defender'},
 
     {id: 7, fname: 'Steph',   lname: 'Houghton',  age: 31, height: '594', caps: 104, goals: 0,
-                    picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/steph-houghton-723.ashx', 
+                    picurl: 'assets/images/steph-houghton-723.png', 
                     club: 'Manchester City', dob:'1993-06-29', team: 'England', position: 'Defender'},
 
     {id: 8, fname: 'Georgia',   lname: 'Stanway',  age: 20, height: '5-3', caps: 7, goals: 0,
-                    picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/georgia-stanway-723.ashx', 
+                    picurl: 'assets/images/georgia-stanway-723.png', 
                     club: 'Manchester City', dob:'1993-06-29', team: 'England', position: 'Midfield'},
 
      {id: 9, fname: 'Jill',   lname: 'Scott',  age: 32, height: '5-11', caps: 51, goals: 0,
-                    picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/jill-scott-723.ashx', 
+                    picurl: 'assets/images/jill-scott-723.png', 
                     club: 'Manchester City', dob:'1993-06-29', team: 'England', position: 'Midfield'},
 
       {id: 10, fname: 'Toni',   lname: 'Duggan',  age: 27, height: '5-6', caps: 71, goals: 0,
-                    picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/toni-duggan-723.ashx', 
+                    picurl: 'assets/images/toni-duggan-723.png', 
                     club: 'Barcelona', dob:'1993-06-29', team: 'England', position: 'Forward'},
 
                     {id: 11, fname: 'Ellen',   lname: 'White',  age: 30, height: '5-7', caps: 81, goals: 0,
-                    picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/ellen-white-723.ashx', 
+                    picurl: 'assets/images/ellen-white-723.png', 
                     club: 'Birmingham City', dob:'1993-06-29', team: 'England', position: 'Forward'},
 
                     {id: 12, fname: 'Karen',   lname: 'Carney',  age: 31, height: '5-4', caps: 139, goals: 0,
-                    picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/karen-carney-723.ashx', 
-                    club: 'Chelsea', dob:'1993-06-29', team: 'England', position: 'Midfield'},
+                    picurl: 'assets/images/karen-carney-723.png', 
+                    club: 'Chelsea', dob:'1993-06-29', team: 'England', position: 'ionic serveMidfield'},
 
                     {id: 13, fname: 'Jodie',   lname: 'Taylor',  age: 33, height: '5-6', caps: 42, goals: 0,
-                    picurl: 'http://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/2019-lionesses-headshots/large-images/jodie-taylor-723.ashx', 
+                    picurl: 'assets/images/jodie-taylor-723.png', 
                     club: 'Reign FC', dob:'1993-06-29', team: 'England', position: 'Forward'}
 
                         
